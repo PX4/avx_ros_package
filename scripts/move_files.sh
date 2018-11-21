51b485cdece5af5a6ac1b354ce32105bb2b77034
Anna Dai <anna@auterion.com>
2018-11-21 17:17:33 +0100

    adding scripts, launch files and support files

--- scripts/move_files.sh
+++ scripts/move_files.sh
@@ -6,13 +6,19 @@
 
 INSTALL_DIR=$PWD
 
-# kalibr wrapper which supports the extrinsics calibration between camera and IMU
-
 # bag folder needed for the calibration in kalibr
+
+# kalibr wrapper which supports the extrinsics calibration between camera and IMU
+scp rovio_footer_px4.txt nvidia@192.168.1.117:/home/nvidia/catkin_ws/src/Kalibr/aslam_offline_calibration/kalibr/python/exporters/auxiliary_files/
+
+# patch footer needed for rovio file
+
 
 
 
 # rovio launch files
 cp $INSTALL_DIR/launch/rovio/.*launch $AUTERION_WS/src/rovio/launch/
+mkdir -p $AUTERION_WS/src/rovio/cfg/cam_intrinsics
+scp rovio_rs_infra1_D435.launch nvidia@192.168.1.117:/home/nvidia/catkin_ws/src/rovio/launch/
 # realsense launch files
 
